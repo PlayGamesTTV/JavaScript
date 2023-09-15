741c1d449e53967852038d866df1859760c4ffe9
PlayGamesTTV <spade.nasa@gmail.com>
2023-09-15 11:26:11 +0200

    JavaScript
    Exercises and explanation in javascript

---
+++ JavaScriptEsercizi/codice.js
@@ -0,0 +1,7 @@
+function init(){
+	document.getElementById("mioP").innerHTML=<em> testo cambiato 3 </em>;
+    document.getElementById("mioLabel").innerHTML=<strong> testo cambiato 4 </strong>;
+    document.getElementById("mioSpan").innerHTML=<b> testo cambiato 2 </b>;
+    document.getElementById("mioDiv").innerHTML=<i> testo cambiato 1 "</i>;
+    document.getElementById("Secondo").innerHTML=document.getElementById("Primo").innerHTML;
+}
